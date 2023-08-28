4ab17ad51e6a5be459525213e34e16120ea7c3e7
jyritasa <jyri.tasala@gmail.com>
2023-08-29 00:46:19 +0300

    added ability to remove orders

--- lib/views/orders_view.dart
+++ lib/views/orders_view.dart
@@ -1,9 +1,15 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:northwind/components/orders/orders_breakdown.dart';
 import 'package:northwind/conf/fonts_and_padding.dart';
+import 'package:northwind/conf/logger.dart';
 import 'package:northwind/models/order_model.dart';
 import 'package:northwind/views/shared/view_class.dart';
 import 'package:northwind/views/single_order_view.dart';
+
+import '../controllers/controller_class.dart';
 
 class OrdersView extends StatelessWidget {
   const OrdersView({super.key});
@@ -46,20 +52,62 @@ class OrdersView extends StatelessWidget {
         );
       },
       tileSubTitleWidget: (order) => Text(order.customer?.companyName ?? ""),
-      tileInnerWidget: (order) => Column(
+      tileInnerWidget: (order, models, displayedModels, updateModels) => Column(
         children: [
           orderBreakDownWidget(order),
           const Padding(padding: EdgeInsets.only(bottom: inBetweenPadding)), //
-          ElevatedButton(
-            child: const Text("More Information"),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SingleOrderView(order: order),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                child: const Text("More Information"),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SingleOrderView(order: order),
+                    ),
+                  );
+                },
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
                 ),
-              );
-            },
+                child: Row(
+                  children: [
+                    const Text(
+                      "Delete Order",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    Icon(
+                      Icons.delete_outline_outlined,
+                      color: Colors.red.shade900,
+                    )
+                  ],
+                ),
+                onPressed: () async {
+                  if (kDebugMode) logger.d("Deleting...");
+                  // Make the delete call using your controller
+                  Controller controller =
+                      Controller<Order>(fromJson: Order.fromJson);
+                  bool success = await controller.delete(order.orderId);
+
+                  if (success) {
+                    // If delete call was successful, update both models and displayedModels
+                    models!.removeWhere((o) => o.orderId == order.orderId);
+                    displayedModels
+                        .removeWhere((o) => o.orderId == order.orderId);
+                    updateModels();
+                  } else {
+                    // Handle deletion failure
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Failed to delete order.")),
+                    );
+                  }
+                },
+              ),
+            ],
           ),
         ],
       ),
