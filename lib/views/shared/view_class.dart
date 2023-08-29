1f34af696e54100ff74f5d52f2c9420603a496cf
jyritasa <jyri.tasala@gmail.com>
2023-08-29 12:18:48 +0300

    removed unused import

--- lib/views/shared/view_class.dart
+++ lib/views/shared/view_class.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:northwind/components/activity_indicator.dart';
 import 'package:northwind/components/search_bar.dart';
