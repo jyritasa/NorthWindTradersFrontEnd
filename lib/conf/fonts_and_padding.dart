473afdd578cf8e40e30fe56fb39c6fcec75f6d31
jyritasa <jyri.tasala@gmail.com>
2023-08-29 12:19:39 +0300

    new styles

--- lib/conf/fonts_and_padding.dart
+++ lib/conf/fonts_and_padding.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 const TextStyle bigStyle = TextStyle(fontSize: 18);
 const TextStyle smallStyle = TextStyle(fontSize: 12);
+const TextStyle menuButtonStyle = TextStyle(fontSize: menuIconSize);
+const double menuIconSize = 32;
 const double verticalPadding = 8;
 const double horizontalPadding = 8;
 const double inBetweenPadding = 5;
